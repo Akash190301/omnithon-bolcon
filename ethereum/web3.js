a2b35e6c54bc4496a0f6632273de4fb9c41881bb
Shivani Kanswal <34507110+Shivanikanswal@users.noreply.github.com>
2019-11-16 22:34:32 +0530

    hbh
    jjb

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,6 +1,11 @@
 import Web3 from 'web3';
 
-const web3= new Web3(window.web3.currentProvider);
+let web3;
+if(typeof window!=='undefined'&& typeof window.web3!=='undefined'){
+    //we are in the browser
+}else{
+
+}
 
 
 export default web3;
